28f86fcbc947a4912aaa082dcc1ce0754364f6bb
Christ John Armada <cjs.armada@gmail.com>
2025-12-03 18:07:03 +0800

    Set up Client Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from './Header.jsx'
 import Hero from './Hero.jsx'
+import ClientSection from './ClientSection.jsx'
 
 function App() {
   return (
     <>
       <Header/>
       <Hero/>
+      <ClientSection/>
     </>
     
   );
---
+++ src/ClientSection.jsx
@@ -0,0 +1,7 @@
+function Clients() {
+    return (
+        ""
+    );
+}
+
+export default Clients
